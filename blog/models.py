aa8cb61caf0aa179c854fae53ad568d594a7079a
Head-Shot-53 <denchikmashtack@gmail.com>
2025-05-27 18:23:31 +0300

    додав модель Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'DRAFT'
+        PUBLISHED = 'PB','PUBLISHED'
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts', default=1)
+    body = models.TextField()
+
+    publish = models.DateTimeField(default=timezone.now) # дата публікації
+    created = models.DateTimeField(auto_now_add=True)# дата створення
+    update = models.DateTimeField(auto_now=True) # дата змінення
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+    
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
