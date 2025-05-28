4f293e4482d3425b640148d5b80950a2188b6cbd
Head-Shot-53 <denchikmashtack@gmail.com>
2025-05-28 19:11:52 +0300

    створив свій модельний менеджер

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Model):
+    def get_queryset(self):
+        return super().get_queryset()\
+                        .filter(status=Post.Status.PUBLISHED)
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'DRAFT'
@@ -16,6 +21,9 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)# дата створення
     update = models.DateTimeField(auto_now=True) # дата змінення
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
+
+    objects = models.Manager() # мнеджер який береться за замовчуванням
+    published = PublishedManager() # конкретно-прикладний менеджер
     
     class Meta:
         ordering = ['-publish']
