97257aa78680dd5eb462ce076f52ad2f0af497ca
Head-Shot-53 <denchikmashtack@gmail.com>
2025-06-16 11:06:52 +0300

    в post_detail додав списки з схожими постами та їх сортування

--- blog/views.py
+++ blog/views.py
@@ -7,24 +7,32 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.conf import settings
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
-# def post_list(request):
-#     post_list = Post.published.all()
-#     paginator = Paginator(post_list, 3) # 3 пости на сторінку
-#     page_number = request.GET.get('page', 1)
-#     try:
-#         posts = paginator.page(page_number)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#     return render(request, 'blog/post/list.html', context={'posts':posts})
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blog/post/list.html'
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug = tag_slug)
+        post_list = post_list.filter(tags__in = [tag])
+
+    paginator = Paginator(post_list, 3) # 3 пости на сторінку
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, 'blog/post/list.html', context={'posts':posts, 'tag':tag})
+
+# class PostListView(ListView):
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -37,7 +45,18 @@ def post_detail(request, year, month, day, post):
     comments = post.comments.filter(active = True)
     # форма для коментування користувачами
     form = CommentForm()
-    return render(request, 'blog/post/detail.html',context={'post':post, 'comments':comments, 'form':form})
+
+    # Список схожих постів
+    post_tags_ids = post.tags.values_list('id', flat=True)
+
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+
+    similar_posts = similar_posts.annotate(same_tags = Count('tags')).order_by('-same_tags', '-publish')[:4]
+    
+    return render(request, 'blog/post/detail.html',context={'post':post, 
+                                                            'comments':comments, 
+                                                            'form':form,
+                                                            'similar_posts':similar_posts})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status = Post.Status.PUBLISHED)
